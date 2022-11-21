a6db6eee9c202094d4e1672466b213eff8beb393
Sin Jae Min <smm1224@naver.com>
2022-11-21 12:19:26 +0900

    stage 1 commit

--- hello.py
+++ hello.py
@@ -9,7 +9,10 @@ a = 10
 b = 20
 print(a+b)
 
-num1= int(input('input num1 : '))
-num2=int(input('input num2 : '))
-#f-string
-print(f"{num1}+{num2}={num1+num2}")
+# num1= int(input('input num1 : '))
+# num2=int(input('input num2 :'))
+# #f-string
+# print(f"{num1}+{num2}={num1+num2}")
+
+for i in range(1,11):
+    print(i)
